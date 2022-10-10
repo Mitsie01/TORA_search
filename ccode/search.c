e45d00f710759d8e8c4977e3d2f4f11c9bbfb5f2
Mitchell Kampert <mitch.kampert@gmail.com>
2022-10-10 23:49:51 +0200

    added single rsf iteration

--- ccode/search.c
+++ ccode/search.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
+#include <math.h>
 
-float dt;
-float t;
+double dt;
+unsigned int t;
 unsigned int Z;
-float gamma;
+double gamma;
+double p1;
+double p;
 
 
 int main(){
 
     dt = 0.01;
-    Z = 25;
-    gamma = 0.5;
+    Z = 1;
+    gamma = 0.4;
+    p1 = 10;
 
 
-    for(t = 0; t <= Z; t = t+dt){
+    for(t = 0; t <= (Z/dt); t +=1){
 
-        printf("%.2f\n", t);
+        p = (1-pow((1-gamma), t))/(100/p1);
+        printf("%.16lf - %u\n", p, t);
     }
 
     return 0;
--- python/rsf/main.py
+++ python/rsf/main.py
@@ -1,1 +1,33 @@
 import numpy as np
+
+Z = 25
+delta = 0.01
+
+tspan = int(Z/delta)
+
+field = np.genfromtxt("field.csv", delimiter=",")
+
+id = field[:,0]
+p_value = field[:,1]
+gamma = field[:,2]
+
+init = np.array([50, 25, 25, 75, 125, 50, 100, 150, 125, 250, 125, 250, 100, 125, 175, 50, 125, 50, 50, 25, 50, 100, 100, 100, 100])
+scores = np.array([])
+
+init_score = np.sum(p_value*(1-np.exp(-gamma*(init*delta))))
+
+print(init_score)
+
+for n in range(init.size-1):
+    if init[n] != 0:
+        init[n] = init[n]-1
+        for i in range(init.size-1):
+            if i != n:
+                init[i] = init[i]+1
+                score = np.sum(p_value*(1-np.exp(-gamma*(init*delta))))
+                scores = np.append(scores, score)
+                init[i] = init[i]-1
+    
+        init[n] = init[n]+1
+
+print(np.max(scores))
